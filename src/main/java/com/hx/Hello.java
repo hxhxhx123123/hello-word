810e45c97149408fdbe10f76ce3395f07d8c0d08
reata <reddevil.hjw@gmail.com>
2021-06-20 13:39:21 +0800

    refactor: change to a maven project

---
+++ src/main/java/com/hx/Hello.java
@@ -0,0 +1,9 @@
+package com.hx;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println(StringUtils.lowerCase("Hello World"));
+    }
+}
